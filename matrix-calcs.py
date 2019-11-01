7a6c955da6181d67550007ffaed7ac38bf6bee4f
Anthony Man <anthonyman97@outlook.com>
2019-11-01 11:46:02 +0000

    commit-1 01/11

--- matrix-calcs.py
+++ matrix-calcs.py
@@ -1,9 +1,22 @@
 import numpy as np
+import math
 
-a = np.array([[1, 0], [0, 1]])
+ECT = [[1, 1, 2], [2, 1, 3], [3, 2, 4], [4, 3, 4], [5, 3, 5], [6, 4, 6], [7, 5, 6],
+                [8, 5, 7], [9, 6, 8], [10, 7, 8], [11, 7, 9], [12, 8, 10], [13, 9, 10]]
 
-det = np.linalg.det(a)
+ArrayECT = np.array(ECT, dtype=np.int32)
 
-print(det)
+ToNC = [[1, 0, 0], [2, 0, 0.3], [3, 1.5, 0], [4, 1.5, 0.25], [5, 3, 0], [6, 3, 0.2], [7, 4.5, 0],
+        [8, 4.5, 0.15], [9, 6, 0], [10, 6, 0.1]]
+
+Array = np.array(ToNC, dtype=np.float32)
+
+L = 1.5
+Beta = 2*(2*math.pi/360)
+
+print(ArrayECT)
+print(ArrayECT.shape)
+
+
 
 
