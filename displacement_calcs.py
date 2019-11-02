2929e18390d62eed89cf48e26978ba7553e4ddf8
Anthony Man <anthonyman97@outlook.com>
2019-11-02 21:54:43 +0000

    commit-5 02/11

---
+++ displacement_calcs.py
@@ -0,0 +1,8 @@
+import global_stiffness_matrix as gsm
+import numpy as np
+
+d = np.array([d1x, d1y, d2x, d2y, d3x, d3y, d4x, d4y, d5x, d5y,
+              d6x, d6y, d7x, d7y, d8x, d8y, d9x, d9y, d10x, d10y
+]). reshape(1,20)
+
+print(d)
