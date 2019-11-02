0ddafec915eebe89574828b7d55d75fbfb516d91
Anthony Man <anthonyman97@outlook.com>
2019-11-02 11:32:03 +0000

    commit-2 02/1

--- local_stiffness_matrices.py
+++ local_stiffness_matrices.py
@@ -1,6 +1,31 @@
-import ("matrix-calcs1.1")
+import nodes_and_elems as nae
 
+E = 200**9
+A = 1**-2
+L = 1.5
 
+local1 = [[nae.l1 ** 2, nae.l1 * nae.m1, -(nae.l1 ** 2),
+           -(nae.l1 * nae.m1)], [nae.l1 * nae.m1, nae.m1 ** 2,
+                               -(nae.l1 * nae.m1), -(nae.m1 ** 2)], [
+              -(nae.l1 ** 2), -(nae.l1 * nae.m1), nae.l1 ** 2,
+              nae.l1 * nae.m1], [-(nae.l1 * nae.m1), -(nae.m1 ** 2),
+                               nae.l1 * nae.m1, nae.m1 ** 2]]
 
+local1 = [[nae.l1 ** 2, nae.l1 * nae.m1, -(nae.l1 ** 2),
+           -(nae.l1 * nae.m1)], [nae.l1 * nae.m1, nae.m1 ** 2,
+                               -(nae.l1 * nae.m1), -(nae.m1 ** 2)], [
+              -(nae.l1 ** 2), -(nae.l1 * nae.m1), nae.l1 ** 2,
+              nae.l1 * nae.m1], [-(nae.l1 * nae.m1), -(nae.m1 ** 2),
+                               nae.l1 * nae.m1, nae.m1 ** 2]]
 
+k1 = (E*A/L)*local1
+
+local2 = [[nae.l2 ** 2, nae.l2 * nae.m2, -(nae.l2 ** 2),
+           -(nae.l2 * nae.m2)], [nae.l2 * nae.m2, nae.m2 ** 2,
+                             -(nae.l2 * nae.m2), -(nae.m2 ** 2)], [
+              -(nae.l2 ** 2), -(nae.l2 * nae.m2), nae.l2 ** 2,
+    nae.l2 * nae.m2], [-(nae.l2 * nae.m2), -(nae.m2 ** 2), nae.l2 * nae.m2, nae.m2
+                                                                 ** 2]]
+
+print(k1)
 
