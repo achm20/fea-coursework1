7b33cb7cc4bf53fc0a47716f6593bcf1470b7ba1
Anthony Man <anthonyman97@outlook.com>
2019-11-02 10:29:09 +0000

    commit-1 02/1

--- matrix-calcs1.1.py
+++ matrix-calcs1.1.py
@@ -30,4 +30,32 @@ Elem_11 = {'n1': Node_7, 'n2': Node_9, 'length': L}
 Elem_12 = {'n1': Node_8, 'n2': Node_10, 'length': L*cb}
 Elem_13 = {'n1': Node_9, 'n2': Node_10, 'length': (1/15)*L}
 
-print(Elem_1)
+l1 = (Elem_1['n2']['x'] - Elem_1['n1']['x'])/(Elem_1['length'])
+l2 = (Elem_2['n2']['x'] - Elem_2['n1']['x'])/(Elem_2['length'])
+l3 = (Elem_3['n2']['x'] - Elem_3['n1']['x'])/(Elem_3['length'])
+l4 = (Elem_4['n2']['x'] - Elem_4['n1']['x'])/(Elem_4['length'])
+l5 = (Elem_5['n2']['x'] - Elem_5['n1']['x'])/(Elem_5['length'])
+l6 = (Elem_6['n2']['x'] - Elem_6['n1']['x'])/(Elem_6['length'])
+l7 = (Elem_7['n2']['x'] - Elem_7['n1']['x'])/(Elem_7['length'])
+l8 = (Elem_8['n2']['x'] - Elem_8['n1']['x'])/(Elem_8['length'])
+l9 = (Elem_9['n2']['x'] - Elem_9['n1']['x'])/(Elem_9['length'])
+l10 = (Elem_10['n2']['x'] - Elem_10['n1']['x'])/(Elem_10['length'])
+l11 = (Elem_11['n2']['x'] - Elem_11['n1']['x'])/(Elem_11['length'])
+l12 = (Elem_12['n2']['x'] - Elem_12['n1']['x'])/(Elem_12['length'])
+l13 = (Elem_13['n2']['x'] - Elem_13['n1']['x'])/(Elem_13['length'])
+
+m1 = (Elem_1['n2']['y'] - Elem_1['n1']['y']) / (Elem_1['length'])
+m2 = (Elem_2['n2']['y'] - Elem_2['n1']['y']) / (Elem_2['length'])
+m3 = (Elem_3['n2']['y'] - Elem_3['n1']['y']) / (Elem_3['length'])
+m4 = (Elem_4['n2']['y'] - Elem_4['n1']['y']) / (Elem_4['length'])
+m5 = (Elem_5['n2']['y'] - Elem_5['n1']['y']) / (Elem_5['length'])
+m6 = (Elem_6['n2']['y'] - Elem_6['n1']['y']) / (Elem_6['length'])
+m7 = (Elem_7['n2']['y'] - Elem_7['n1']['y']) / (Elem_7['length'])
+m8 = (Elem_8['n2']['y'] - Elem_8['n1']['y']) / (Elem_8['length'])
+m9 = (Elem_9['n2']['y'] - Elem_9['n1']['y']) / (Elem_9['length'])
+m10 = (Elem_10['n2']['y'] - Elem_10['n1']['y']) / (Elem_10['length'])
+m11 = (Elem_11['n2']['y'] - Elem_11['n1']['y']) / (Elem_11['length'])
+m12 = (Elem_12['n2']['y'] - Elem_12['n1']['y']) / (Elem_12['length'])
+m13 = (Elem_13['n2']['y'] - Elem_13['n1']['y']) / (Elem_13['length'])
+
+print(m8)
