6763dba0e97dc456d12e19eef200ae233500b4ab
Anthony Man <anthonyman97@outlook.com>
2019-11-02 20:33:20 +0000

    commit-4 02/11

--- global_stiffness_matrix.py
+++ global_stiffness_matrix.py
@@ -2,16 +2,19 @@ import nodes_and_elems as nae
 import local_stiffness_matrices as lsm
 import numpy as np
 
-M = np.zeros((6,6))
+M = np.zeros((20,20))
 M[:4, :4] += lsm.k1
 M[np.ix_([0, 1, 4, 5], [0, 1, 4, 5])] += lsm.k2
 M[np.ix_([2, 3, 6, 7], [2, 3, 6, 7])] += lsm.k3
 M[4:8, 4:8] += lsm.k4
 M[np.ix_([4, 5, 8, 9], [4, 5, 8, 9])] += lsm.k5
-
+M[np.ix_([6, 7, 10, 11], [6, 7, 10, 11])] += lsm.k6
+M[8:12, 8:12] += lsm.k7
+M[np.ix_([8, 9, 12, 13], [8, 9, 12, 13])] += lsm.k8
+M[np.ix_([10, 11, 14, 15], [10, 11, 14, 15])] += lsm.k9
+M[12:16, 12:16] += lsm.k10
+M[np.ix_([12, 13, 16, 17], [12, 13, 16, 17])] += lsm.k11
+M[np.ix_([14, 15, 18, 19], [14, 15, 18, 19])] += lsm.k12
+M[16:20, 16:20] += lsm.k13
 
 print(M)
-
-#M[:2,:2] += M1
-#M[1:3, 1:3] += M2
-
