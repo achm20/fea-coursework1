0c7e53408efe835761afd5be87a79386a324fc87
Anthony Man <anthonyman97@outlook.com>
2019-11-05 16:43:19 +0000

    commit-2 05/11

--- local_stiffness_matrices.py
+++ local_stiffness_matrices.py
@@ -19,25 +19,25 @@ k_rod11 = A*E/nae.Elem_11['length']
 k_rod12 = A*E/nae.Elem_12['length']
 k_rod13 = A*E/nae.Elem_13['length']
 
-local1 = [[nae.l1 ** 2, nae.l1 * nae.m1, -(nae.l1 ** 2),
+global1 = [[nae.l1 ** 2, nae.l1 * nae.m1, -(nae.l1 ** 2),
            -(nae.l1 * nae.m1)], [nae.l1 * nae.m1, nae.m1 ** 2,
                                -(nae.l1 * nae.m1), -(nae.m1 ** 2)], [
               -(nae.l1 ** 2), -(nae.l1 * nae.m1), nae.l1 ** 2,
               nae.l1 * nae.m1], [-(nae.l1 * nae.m1), -(nae.m1 ** 2),
                                nae.l1 * nae.m1, nae.m1 ** 2]]
 
-k1 = k_rod1*np.array(local1)
+k1 = k_rod1*np.array(global1)
 
-local2 = [[nae.l2 ** 2, nae.l2 * nae.m2, -(nae.l2 ** 2),
+global2 = [[nae.l2 ** 2, nae.l2 * nae.m2, -(nae.l2 ** 2),
            -(nae.l2 * nae.m2)], [nae.l2 * nae.m2, nae.m2 ** 2,
                              -(nae.l2 * nae.m2), -(nae.m2 ** 2)], [
               -(nae.l2 ** 2), -(nae.l2 * nae.m2), nae.l2 ** 2,
     nae.l2 * nae.m2], [-(nae.l2 * nae.m2), -(nae.m2 ** 2),
                        nae.l2 * nae.m2, nae.m2 ** 2]]
 
-k2 = k_rod2*np.array(local2)
+k2 = k_rod2*np.array(global2)
 
-local3 = [[nae.l3 ** 2, nae.l3 * nae.m3, -(nae.l3 ** 2),
+global3 = [[nae.l3 ** 2, nae.l3 * nae.m3, -(nae.l3 ** 2),
            -(nae.l3 * nae.m3)], [nae.l3 * nae.m3, nae.m3 ** 2,
                              -(nae.l3 * nae.m3), -(nae.m3 ** 2)], [
               -(nae.l3 ** 2), -(nae.l3 * nae.m3), nae.l3 ** 2,
@@ -45,9 +45,9 @@ local3 = [[nae.l3 ** 2, nae.l3 * nae.m3, -(nae.l3 ** 2),
         nae.l3 * nae.m3), -(nae.m3 ** 2), nae.l3 * nae.m3, nae.m3
                                                              ** 2]]
 
-k3 = k_rod3*np.array(local3)
+k3 = k_rod3*np.array(global3)
 
-local4 = [[nae.l4 ** 2, nae.l4 * nae.m4, -(nae.l4 ** 2),
+global4 = [[nae.l4 ** 2, nae.l4 * nae.m4, -(nae.l4 ** 2),
            -(nae.l4 * nae.m4)], [nae.l4 * nae.m4, nae.m4 ** 2,
                              -(nae.l4 * nae.m4), -(nae.m4 ** 2)], [
               -(nae.l4 ** 2), -(nae.l4 * nae.m4), nae.l4 ** 2,
@@ -55,9 +55,9 @@ local4 = [[nae.l4 ** 2, nae.l4 * nae.m4, -(nae.l4 ** 2),
         nae.l4 * nae.m4), -(nae.m4 ** 2), nae.l4 * nae.m4, nae.m4
                                                              ** 2]]
 
-k4 = k_rod4*np.array(local4)
+k4 = k_rod4*np.array(global4)
 
-local5 = [[nae.l5 ** 2, nae.l5 * nae.m5, -(nae.l5 ** 2),
+global5 = [[nae.l5 ** 2, nae.l5 * nae.m5, -(nae.l5 ** 2),
            -(nae.l5 * nae.m5)], [nae.l5 * nae.m5, nae.m5 ** 2,
                              -(nae.l5 * nae.m5), -(nae.m5 ** 2)], [
               -(nae.l5 ** 2), -(nae.l5 * nae.m5), nae.l5 ** 2,
@@ -65,9 +65,9 @@ local5 = [[nae.l5 ** 2, nae.l5 * nae.m5, -(nae.l5 ** 2),
         nae.l5 * nae.m5), -(nae.m5 ** 2), nae.l5 * nae.m5, nae.m5
                                                              ** 2]]
 
-k5 = k_rod5*np.array(local5)
+k5 = k_rod5*np.array(global5)
 
-local6 = [[nae.l6 ** 2, nae.l6 * nae.m6, -(nae.l6 ** 2),
+global6 = [[nae.l6 ** 2, nae.l6 * nae.m6, -(nae.l6 ** 2),
            -(nae.l6 * nae.m6)], [nae.l6 * nae.m6, nae.m6 ** 2,
                              -(nae.l6 * nae.m6), -(nae.m6 ** 2)], [
               -(nae.l6 ** 2), -(nae.l6 * nae.m6), nae.l6 ** 2,
@@ -75,9 +75,9 @@ local6 = [[nae.l6 ** 2, nae.l6 * nae.m6, -(nae.l6 ** 2),
         nae.l6 * nae.m6), -(nae.m6 ** 2), nae.l6 * nae.m6, nae.m6
                                                              ** 2]]
 
-k6 = k_rod6*np.array(local6)
+k6 = k_rod6*np.array(global6)
 
-local7 = [[nae.l7 ** 2, nae.l7 * nae.m7, -(nae.l7 ** 2),
+global7 = [[nae.l7 ** 2, nae.l7 * nae.m7, -(nae.l7 ** 2),
            -(nae.l7 * nae.m7)], [nae.l7 * nae.m7, nae.m7 ** 2,
                              -(nae.l7 * nae.m7), -(nae.m7 ** 2)], [
               -(nae.l7 ** 2), -(nae.l7 * nae.m7), nae.l7 ** 2,
@@ -85,9 +85,9 @@ local7 = [[nae.l7 ** 2, nae.l7 * nae.m7, -(nae.l7 ** 2),
         nae.l7 * nae.m7), -(nae.m7 ** 2), nae.l7 * nae.m7, nae.m7
                                                              ** 2]]
 
-k7 = k_rod7*np.array(local7)
+k7 = k_rod7*np.array(global7)
 
-local8 = [[nae.l8 ** 2, nae.l8 * nae.m8, -(nae.l8 ** 2),
+global8 = [[nae.l8 ** 2, nae.l8 * nae.m8, -(nae.l8 ** 2),
            -(nae.l8 * nae.m8)], [nae.l8 * nae.m8, nae.m8 ** 2,
                              -(nae.l8 * nae.m8), -(nae.m8 ** 2)], [
               -(nae.l8 ** 2), -(nae.l8 * nae.m8), nae.l8 ** 2,
@@ -95,9 +95,9 @@ local8 = [[nae.l8 ** 2, nae.l8 * nae.m8, -(nae.l8 ** 2),
         nae.l8 * nae.m8), -(nae.m8 ** 2), nae.l8 * nae.m8, nae.m8
                                                              ** 2]]
 
-k8 = k_rod8*np.array(local8)
+k8 = k_rod8*np.array(global8)
 
-local9 = [[nae.l9 ** 2, nae.l9 * nae.m9, -(nae.l9 ** 2),
+global9 = [[nae.l9 ** 2, nae.l9 * nae.m9, -(nae.l9 ** 2),
            -(nae.l9 * nae.m9)], [nae.l9 * nae.m9, nae.m9 ** 2,
                              -(nae.l9 * nae.m9), -(nae.m9 ** 2)], [
               -(nae.l9 ** 2), -(nae.l9 * nae.m9), nae.l9 ** 2,
@@ -105,9 +105,9 @@ local9 = [[nae.l9 ** 2, nae.l9 * nae.m9, -(nae.l9 ** 2),
         nae.l9 * nae.m9), -(nae.m9 ** 2), nae.l9 * nae.m9, nae.m9
                                                              ** 2]]
 
-k9 = k_rod9*np.array(local9)
+k9 = k_rod9*np.array(global9)
 
-local10 = [[nae.l10 ** 2, nae.l10 * nae.m10, -(nae.l10 ** 2),
+global10 = [[nae.l10 ** 2, nae.l10 * nae.m10, -(nae.l10 ** 2),
             -(nae.l10 * nae.m10)], [nae.l10 * nae.m10, nae.m10 ** 2,
                                 -(nae.l10 * nae.m10), -(nae.m10 **
                                                          2)], [
@@ -116,9 +116,9 @@ local10 = [[nae.l10 ** 2, nae.l10 * nae.m10, -(nae.l10 ** 2),
     nae.l10 * nae.m10), -(nae.m10 ** 2), nae.l10 * nae.m10, nae.m10
                                                            ** 2]]
 
-k10 = k_rod10*np.array(local10)
+k10 = k_rod10*np.array(global10)
 
-local11 = [[nae.l11 ** 2, nae.l11 * nae.m11, -(nae.l11 ** 2),
+global11 = [[nae.l11 ** 2, nae.l11 * nae.m11, -(nae.l11 ** 2),
             -(nae.l11 * nae.m11)], [nae.l11 * nae.m11, nae.m11 ** 2,
                                -(nae.l11 * nae.m11), -(nae.m11 **
                                                        2)], [
@@ -127,9 +127,9 @@ local11 = [[nae.l11 ** 2, nae.l11 * nae.m11, -(nae.l11 ** 2),
            [-(nae.l11 * nae.m11), -(nae.m11 ** 2), nae.l11 *
             nae.m11, nae.m11 ** 2]]
 
-k11 = k_rod11*np.array(local11)
+k11 = k_rod11*np.array(global11)
 
-local12 = [[nae.l12 ** 2, nae.l12 * nae.m12, -(nae.l12 ** 2),
+global12 = [[nae.l12 ** 2, nae.l12 * nae.m12, -(nae.l12 ** 2),
             -(nae.l12 * nae.m12)], [nae.l12 * nae.m12, nae.m12 ** 2,
                                -(nae.l12 * nae.m12), -(nae.m12 ** 2)], [
               -(nae.l12 ** 2), -(nae.l12 * nae.m12), nae.l12 ** 2,
@@ -137,9 +137,9 @@ local12 = [[nae.l12 ** 2, nae.l12 * nae.m12, -(nae.l12 ** 2),
         nae.l12 * nae.m12), -(nae.m12 ** 2), nae.l12 * nae.m12, nae.m12
                                                                 ** 2]]
 
-k12 = k_rod12*np.array(local12)
+k12 = k_rod12*np.array(global12)
 
-local13 = [[nae.l13 ** 2, nae.l13 * nae.m13, -(nae.l13 ** 2),
+global13 = [[nae.l13 ** 2, nae.l13 * nae.m13, -(nae.l13 ** 2),
             -(nae.l13 * nae.m13)], [nae.l13 * nae.m13, nae.m13 ** 2,
                                -(nae.l13 * nae.m13), -(nae.m13 ** 2)], [
               -(nae.l13 ** 2), -(nae.l13 * nae.m13), nae.l13 ** 2,
@@ -147,4 +147,4 @@ local13 = [[nae.l13 ** 2, nae.l13 * nae.m13, -(nae.l13 ** 2),
         nae.l13 * nae.m13), -(nae.m13 ** 2), nae.l13 * nae.m13, nae.m13
                                                                 ** 2]]
 
-k13 = k_rod13*np.array(local13)
+k13 = k_rod13*np.array(global13)
--- local_stiffness_matrices_frames.py
+++ local_stiffness_matrices_frames.py
@@ -8,33 +8,239 @@ d = 0.1
 I = (b*d**3)/12
 L = 1.5
 
-k_rod1 = A*E/nae.Elem_1['length']
-k_rod2 = A*E/nae.Elem_2['length']
-k_rod3 = A*E/nae.Elem_3['length']
-k_rod4 = A*E/nae.Elem_4['length']
-k_rod5 = A*E/nae.Elem_5['length']
-k_rod6 = A*E/nae.Elem_6['length']
-k_rod7 = A*E/nae.Elem_7['length']
-k_rod8 = A*E/nae.Elem_8['length']
-k_rod9 = A*E/nae.Elem_9['length']
-k_rod10 = A*E/nae.Elem_10['length']
-k_rod11 = A*E/nae.Elem_11['length']
-k_rod12 = A*E/nae.Elem_12['length']
-k_rod13 = A*E/nae.Elem_13['length']
+length1 = nae.Elem_1['length']
+length2 = nae.Elem_2['length']
+length3 = nae.Elem_3['length']
+length4 = nae.Elem_4['length']
+length5 = nae.Elem_5['length']
+length6 = nae.Elem_6['length']
+length7 = nae.Elem_7['length']
+length8 = nae.Elem_8['length']
+length9 = nae.Elem_9['length']
+length10 = nae.Elem_10['length']
+length11 = nae.Elem_11['length']
+length12 = nae.Elem_12['length']
+length13 = nae.Elem_13['length']
 
-k_beam1 = E*I/((nae.Elem_1['length'])**3)
-k_beam2 = E*I/((nae.Elem_2['length'])**3)
-k_beam3 = E*I/((nae.Elem_3['length'])**3)
-k_beam4 = E*I/((nae.Elem_4['length'])**3)
-k_beam5 = E*I/((nae.Elem_5['length'])**3)
-k_beam6 = E*I/((nae.Elem_6['length'])**3)
-k_beam7 = E*I/((nae.Elem_7['length'])**3)
-k_beam8 = E*I/((nae.Elem_8['length'])**3)
-k_beam9 = E*I/((nae.Elem_9['length'])**3)
-k_beam10 = E*I/((nae.Elem_10['length'])**3)
-k_beam11 = E*I/((nae.Elem_11['length'])**3)
-k_beam12 = E*I/((nae.Elem_12['length'])**3)
-k_beam13 = E*I/((nae.Elem_13['length'])**3)
+k_rod1 = A*E/length1
+k_rod2 = A*E/length2
+k_rod3 = A*E/length3
+k_rod4 = A*E/length4
+k_rod5 = A*E/length5
+k_rod6 = A*E/length6
+k_rod7 = A*E/length7
+k_rod8 = A*E/length8
+k_rod9 = A*E/length9
+k_rod10 = A*E/length10
+k_rod11 = A*E/length11
+k_rod12 = A*E/length12
+k_rod13 = A*E/length13
+
+k_beam1 = E*I/length1**3
+k_beam2 = E*I/length2**3
+k_beam3 = E*I/length3**3
+k_beam4 = E*I/length4**3
+k_beam5 = E*I/length5**3
+k_beam6 = E*I/length6**3
+k_beam7 = E*I/length7**3
+k_beam8 = E*I/length8**3
+k_beam9 = E*I/length9**3
+k_beam10 = E*I/length10**3
+k_beam11 = E*I/length11**3
+k_beam12 = E*I/length12**3
+k_beam13 = E*I/length13**3
+
+k_frame_local_1 = [[k_rod1, 0, 0, -k_rod1, 0, 0],
+            [0, 12*k_beam1, 6*length1*k_beam1, 0, -12*k_beam1,
+             6*length1*k_beam1],
+            [0, 6*length1*k_beam1, 4*(length1**2)*k_beam1, 0,
+             -6*length1*k_beam1, 2*(length1**2)*k_beam1],
+            [-k_rod1, 0, 0, k_rod1, 0, 0],
+            [0, -12*k_beam1, -6*length1*k_beam1, 0, 12*k_beam1,
+             -6*length1*k_beam1],
+            [0, 6*length1*k_beam1, 2*(length1**2)*k_beam1, 0,
+             -6*length1*k_beam1, 4*(length1**2)*k_beam1]]
+
+k_frame_local_2 = [[k_rod2, 0, 0, -k_rod2, 0, 0],
+                   [0, 12 * k_beam2, 6 * length2 * k_beam2, 0, -12 * k_beam2,
+                    6 * length2 * k_beam2],
+                   [0, 6 * length2 * k_beam2, 4 * (
+                               length2 ** 2) * k_beam2, 0,
+                    -6 * length2 * k_beam2, 2 * (
+                                length2 ** 2) * k_beam2],
+                   [-k_rod2, 0, 0, k_rod2, 0, 0],
+                   [0, -12 * k_beam2, -6 * length2 * k_beam2, 0, 12 * k_beam2,
+                    -6 * length2 * k_beam2],
+                   [0, 6 * length2 * k_beam2, 2 * (length2 ** 2) *
+                    k_beam2, 0,
+                    -6 * length2 * k_beam2, 4 * (length2 ** 2) *
+                    k_beam2]]
+
+k_frame_local_3 = [[k_rod3, 0, 0, -k_rod3, 0, 0],
+                   [0, 12 * k_beam3, 6 * length3 * k_beam3, 0, -12 * k_beam3,
+                    6 * length3 * k_beam3],
+                   [0, 6 * length3 * k_beam3, 4 * (
+                           length3 ** 2) * k_beam3, 0,
+                    -6 * length3 * k_beam3, 2 * (
+                            length3 ** 2) * k_beam3],
+                   [-k_rod3, 0, 0, k_rod3, 0, 0],
+                   [0, -12 * k_beam3, -6 * length3 * k_beam3, 0, 12 * k_beam3,
+                    -6 * length3 * k_beam3],
+                   [0, 6 * length3 * k_beam3, 2 * (length3 ** 2) *
+                    k_beam3, 0,
+                    -6 * length3 * k_beam3, 4 * (length3 ** 2) *
+                    k_beam3]]
+
+k_frame_local_4 = [[k_rod4, 0, 0, -k_rod4, 0, 0],
+                   [0, 12 * k_beam4, 6 * length4 * k_beam4, 0, -12 * k_beam4,
+                    6 * length4 * k_beam4],
+                   [0, 6 * length4 * k_beam4, 4 * (
+                           length4 ** 2) * k_beam4, 0,
+                    -6 * length4 * k_beam4, 2 * (
+                            length4 ** 2) * k_beam4],
+                   [-k_rod4, 0, 0, k_rod4, 0, 0],
+                   [0, -12 * k_beam4, -6 * length4 * k_beam4, 0, 12 * k_beam4,
+                    -6 * length4 * k_beam4],
+                   [0, 6 * length4 * k_beam4, 2 * (length4 ** 2) *
+                    k_beam4, 0,
+                    -6 * length4 * k_beam4, 4 * (length4 ** 2) *
+                    k_beam4]]
+
+k_frame_local_5 = [[k_rod5, 0, 0, -k_rod5, 0, 0],
+                   [0, 12 * k_beam5, 6 * length5 * k_beam5, 0, -12 * k_beam5,
+                    6 * length5 * k_beam5],
+                   [0, 6 * length5 * k_beam5, 4 * (
+                           length5 ** 2) * k_beam5, 0,
+                    -6 * length5 * k_beam5, 2 * (
+                            length5 ** 2) * k_beam5],
+                   [-k_rod5, 0, 0, k_rod5, 0, 0],
+                   [0, -12 * k_beam5, -6 * length5 * k_beam5, 0, 12 * k_beam5,
+                    -6 * length5 * k_beam5],
+                   [0, 6 * length5 * k_beam5, 2 * (length5 ** 2) *
+                    k_beam5, 0,
+                    -6 * length5 * k_beam5, 4 * (length5 ** 2) *
+                    k_beam5]]
+
+k_frame_local_6 = [[k_rod6, 0, 0, -k_rod6, 0, 0],
+                   [0, 12 * k_beam6, 6 * length6 * k_beam6, 0, -12 * k_beam6,
+                    6 * length6 * k_beam6],
+                   [0, 6 * length6 * k_beam6, 4 * (
+                           length6 ** 2) * k_beam6, 0,
+                    -6 * length6 * k_beam6, 2 * (
+                            length6 ** 2) * k_beam6],
+                   [-k_rod6, 0, 0, k_rod6, 0, 0],
+                   [0, -12 * k_beam6, -6 * length6 * k_beam6, 0, 12 * k_beam6,
+                    -6 * length6 * k_beam6],
+                   [0, 6 * length6 * k_beam6, 2 * (length6 ** 2) *
+                    k_beam6, 0,
+                    -6 * length6 * k_beam6, 4 * (length6 ** 2) *
+                    k_beam6]]
+
+k_frame_local_7 = [[k_rod7, 0, 0, -k_rod7, 0, 0],
+                   [0, 12 * k_beam7, 6 * length7 * k_beam7, 0, -12 * k_beam7,
+                    6 * length7 * k_beam7],
+                   [0, 6 * length7 * k_beam7, 4 * (
+                           length7 ** 2) * k_beam7, 0,
+                    -6 * length7 * k_beam7, 2 * (
+                            length7 ** 2) * k_beam7],
+                   [-k_rod7, 0, 0, k_rod7, 0, 0],
+                   [0, -12 * k_beam7, -6 * length7 * k_beam7, 0, 12 * k_beam7,
+                    -6 * length7 * k_beam7],
+                   [0, 6 * length7 * k_beam7, 2 * (length7 ** 2) *
+                    k_beam7, 0,
+                    -6 * length7 * k_beam7, 4 * (length7 ** 2) *
+                    k_beam7]]
+
+k_frame_local_8 = [[k_rod8, 0, 0, -k_rod8, 0, 0],
+                   [0, 12 * k_beam8, 6 * length8 * k_beam8, 0, -12 * k_beam8,
+                    6 * length8 * k_beam8],
+                   [0, 6 * length8 * k_beam8, 4 * (
+                           length8 ** 2) * k_beam8, 0,
+                    -6 * length8 * k_beam8, 2 * (
+                            length8 ** 2) * k_beam8],
+                   [-k_rod8, 0, 0, k_rod8, 0, 0],
+                   [0, -12 * k_beam8, -6 * length8 * k_beam8, 0, 12 * k_beam8,
+                    -6 * length8 * k_beam8],
+                   [0, 6 * length8 * k_beam8, 2 * (length8 ** 2) *
+                    k_beam8, 0,
+                    -6 * length8 * k_beam8, 4 * (length8 ** 2) *
+                    k_beam8]]
+
+k_frame_local_9 = [[k_rod9, 0, 0, -k_rod9, 0, 0],
+                   [0, 12 * k_beam9, 6 * length9 * k_beam9, 0, -12 * k_beam9,
+                    6 * length9 * k_beam9],
+                   [0, 6 * length9 * k_beam9, 4 * (
+                           length9 ** 2) * k_beam9, 0,
+                    -6 * length9 * k_beam9, 2 * (
+                            length9 ** 2) * k_beam9],
+                   [-k_rod9, 0, 0, k_rod9, 0, 0],
+                   [0, -12 * k_beam9, -6 * length9 * k_beam9, 0, 12 * k_beam9,
+                    -6 * length9 * k_beam9],
+                   [0, 6 * length9 * k_beam9, 2 * (length9 ** 2) *
+                    k_beam9, 0,
+                    -6 * length9 * k_beam9, 4 * (length9 ** 2) *
+                    k_beam9]]
+
+k_frame_local_10 = [[k_rod10, 0, 0, -k_rod10, 0, 0],
+                    [0, 12 * k_beam10, 6 * length10 * k_beam10, 0, -12 * k_beam10,
+                     6 * length10 * k_beam10],
+                    [0, 6 * length10 * k_beam10, 4 * (
+                            length10 ** 2) * k_beam10, 0,
+                     -6 * length10 * k_beam10, 2 * (
+                             length10 ** 2) * k_beam10],
+                    [-k_rod10, 0, 0, k_rod10, 0, 0],
+                    [0, -12 * k_beam10, -6 * length10 * k_beam10, 0, 12 * k_beam10,
+                     -6 * length10 * k_beam10],
+                    [0, 6 * length10 * k_beam10, 2 * (length10 ** 2) *
+                     k_beam10, 0,
+                     -6 * length10 * k_beam10, 4 * (length10 ** 2) *
+                     k_beam10]]
+
+k_frame_local_11 = [[k_rod11, 0, 0, -k_rod11, 0, 0],
+                    [0, 12 * k_beam11, 6 * length11 * k_beam11, 0, -12 * k_beam11,
+                     6 * length11 * k_beam11],
+                    [0, 6 * length11 * k_beam11, 4 * (
+                            length11 ** 2) * k_beam11, 0,
+                     -6 * length11 * k_beam11, 2 * (
+                             length11 ** 2) * k_beam11],
+                    [-k_rod11, 0, 0, k_rod11, 0, 0],
+                    [0, -12 * k_beam11, -6 * length11 * k_beam11, 0, 12 * k_beam11,
+                     -6 * length11 * k_beam11],
+                    [0, 6 * length11 * k_beam11, 2 * (length11 ** 2) *
+                     k_beam11, 0,
+                     -6 * length11 * k_beam11, 4 * (length11 ** 2) *
+                     k_beam11]]
+
+k_frame_local_12 = [[k_rod12, 0, 0, -k_rod12, 0, 0],
+                    [0, 12 * k_beam12, 6 * length12 * k_beam12, 0, -12 * k_beam12,
+                     6 * length12 * k_beam12],
+                    [0, 6 * length12 * k_beam12, 4 * (
+                            length12 ** 2) * k_beam12, 0,
+                     -6 * length12 * k_beam12, 2 * (
+                             length12 ** 2) * k_beam12],
+                    [-k_rod12, 0, 0, k_rod12, 0, 0],
+                    [0, -12 * k_beam12, -6 * length12 * k_beam12, 0, 12 * k_beam12,
+                     -6 * length12 * k_beam12],
+                    [0, 6 * length12 * k_beam12, 2 * (length12 ** 2) *
+                     k_beam12, 0,
+                     -6 * length12 * k_beam12, 4 * (length12 ** 2) *
+                     k_beam12]]
+
+k_frame_local_13 = [[k_rod13, 0, 0, -k_rod13, 0, 0],
+                    [0, 12 * k_beam13, 6 * length13 * k_beam13, 0, -12 * k_beam13,
+                     6 * length13 * k_beam13],
+                    [0, 6 * length13 * k_beam13, 4 * (
+                            length13 ** 2) * k_beam13, 0,
+                     -6 * length13 * k_beam13, 2 * (
+                             length13 ** 2) * k_beam13],
+                    [-k_rod13, 0, 0, k_rod13, 0, 0],
+                    [0, -12 * k_beam13, -6 * length13 * k_beam13, 0, 12 * k_beam13,
+                     -6 * length13 * k_beam13],
+                    [0, 6 * length13 * k_beam13, 2 * (length13 ** 2) *
+                     k_beam13, 0,
+                     -6 * length13 * k_beam13, 4 * (length13 ** 2) *
+                     k_beam13]]
+
 
 
 
