881bf465b97d3b23071665769ed7a39ead99f0f6
Anthony Man <anthonyman97@outlook.com>
2019-11-07 10:59:08 +0000

    commit-2 7/11

--- displacement_calcs_frames.py
+++ displacement_calcs_frames.py
@@ -1,5 +1,6 @@
 import global_stiffness_construct_frames as gscf
 import global_stiffness_matrices_frames as gsmf
+import nodes_and_elems as nae
 import numpy as np
 from numpy.linalg import inv
 
@@ -53,8 +54,9 @@ F = np.array([f3x, f3y, m3, f4x, f4y, m4, f5x, f5y, m5, f6x, f6y,
 
 M3 = inv(M2)
 D = np.matmul(M3, F)
-#print(D)
+print(D)
 
+#--------------------------------------------------------------------
 #Local displacement calcs
 
 #Element 2
@@ -101,67 +103,135 @@ local_disp_el6 = np.matmul(gsmf.Transform6, T_global_disp_vector_el6)
 
 
 #Element 7
-global_disp_vector_el5 = np.array([D[0, 0], D[1, 0], D[2, 0], D[6,
+global_disp_vector_el7 = np.array([D[6, 0], D[7, 0], D[8, 0], D[9,
                                                                 0],
-                                   D[7, 0], D[8, 0]])
-T_global_disp_vector_el5 = np.array(
-    global_disp_vector_el5).reshape(6, 1)
-local_disp_el5 = np.matmul(gsmf.Transform5, T_global_disp_vector_el5)
+                                   D[10, 0], D[11, 0]])
+T_global_disp_vector_el7 = np.array(
+    global_disp_vector_el7).reshape(6, 1)
+local_disp_el7 = np.matmul(gsmf.Transform7, T_global_disp_vector_el7)
 
 
 #Element 8
-global_disp_vector_el5 = np.array([D[0, 0], D[1, 0], D[2, 0], D[6,
+global_disp_vector_el8 = np.array([D[6, 0], D[7, 0], D[8, 0], D[12,
                                                                 0],
-                                   D[7, 0], D[8, 0]])
-T_global_disp_vector_el5 = np.array(
-    global_disp_vector_el5).reshape(6, 1)
-local_disp_el5 = np.matmul(gsmf.Transform5, T_global_disp_vector_el5)
+                                   D[13, 0], D[14, 0]])
+T_global_disp_vector_el8 = np.array(
+    global_disp_vector_el8).reshape(6, 1)
+local_disp_el8 = np.matmul(gsmf.Transform8, T_global_disp_vector_el8)
 
 
 #Element 9
-global_disp_vector_el5 = np.array([D[0, 0], D[1, 0], D[2, 0], D[6,
+global_disp_vector_el9 = np.array([D[9, 0], D[10, 0], D[11, 0], D[15,
                                                                 0],
-                                   D[7, 0], D[8, 0]])
-T_global_disp_vector_el5 = np.array(
-    global_disp_vector_el5).reshape(6, 1)
-local_disp_el5 = np.matmul(gsmf.Transform5, T_global_disp_vector_el5)
+                                   D[16, 0], D[17, 0]])
+T_global_disp_vector_el9 = np.array(
+    global_disp_vector_el9).reshape(6, 1)
+local_disp_el9 = np.matmul(gsmf.Transform9, T_global_disp_vector_el9)
 
 
 #Element 10
-global_disp_vector_el5 = np.array([D[0, 0], D[1, 0], D[2, 0], D[6,
+global_disp_vector_el10 = np.array([D[12, 0], D[13, 0], D[14, 0],
+                                    D[15,
                                                                 0],
-                                   D[7, 0], D[8, 0]])
-T_global_disp_vector_el5 = np.array(
-    global_disp_vector_el5).reshape(6, 1)
-local_disp_el5 = np.matmul(gsmf.Transform5, T_global_disp_vector_el5)
+                                   D[16, 0], D[17, 0]])
+T_global_disp_vector_el10 = np.array(
+    global_disp_vector_el10).reshape(6, 1)
+local_disp_el10 = np.matmul(gsmf.Transform10,
+                            T_global_disp_vector_el10)
 
 
 #Element 11
-global_disp_vector_el5 = np.array([D[0, 0], D[1, 0], D[2, 0], D[6,
+global_disp_vector_el11 = np.array([D[12, 0], D[13, 0], D[14, 0],
+                                    D[18,
                                                                 0],
-                                   D[7, 0], D[8, 0]])
-T_global_disp_vector_el5 = np.array(
-    global_disp_vector_el5).reshape(6, 1)
-local_disp_el5 = np.matmul(gsmf.Transform5, T_global_disp_vector_el5)
+                                   D[19, 0], D[20, 0]])
+T_global_disp_vector_el11 = np.array(
+    global_disp_vector_el11).reshape(6, 1)
+local_disp_el11 = np.matmul(gsmf.Transform11,
+                            T_global_disp_vector_el11)
 
 
 #Element 12
-global_disp_vector_el5 = np.array([D[0, 0], D[1, 0], D[2, 0], D[6,
+global_disp_vector_el12 = np.array([D[15, 0], D[16, 0], D[17, 0],
+                                    D[21,
                                                                 0],
-                                   D[7, 0], D[8, 0]])
-T_global_disp_vector_el5 = np.array(
-    global_disp_vector_el5).reshape(6, 1)
-local_disp_el5 = np.matmul(gsmf.Transform5, T_global_disp_vector_el5)
+                                   D[22, 0], D[23, 0]])
+T_global_disp_vector_el12 = np.array(
+    global_disp_vector_el12).reshape(6, 1)
+local_disp_el12 = np.matmul(gsmf.Transform12,
+                            T_global_disp_vector_el12)
 
 
 #Element 13
-global_disp_vector_el5 = np.array([D[0, 0], D[1, 0], D[2, 0], D[6,
+global_disp_vector_el13 = np.array([D[18, 0], D[19, 0], D[20, 0],
+                                    D[21,
                                                                 0],
-                                   D[7, 0], D[8, 0]])
-T_global_disp_vector_el5 = np.array(
-    global_disp_vector_el5).reshape(6, 1)
-local_disp_el5 = np.matmul(gsmf.Transform5, T_global_disp_vector_el5)
+                                   D[22, 0], D[23, 0]])
+T_global_disp_vector_el13 = np.array(
+    global_disp_vector_el13).reshape(6, 1)
+local_disp_el13 = np.matmul(gsmf.Transform13,
+                            T_global_disp_vector_el13)
+
+#--------------------------------------------------------------------
+#Element Strain calcs
+
+#Element 2 strain
+e_el2 = (local_disp_el2[3, 0] - local_disp_el2[0, 0])/nae.Elem_2[
+    'length']
 
 
-print(D)
+#Element 3 strain
+e_el3 = (local_disp_el3[3, 0] - local_disp_el3[0, 0])/nae.Elem_3[
+    'length']
+
+
+#Element 4 strain
+e_el4 = (local_disp_el4[3, 0] - local_disp_el4[0, 0])/nae.Elem_4[
+    'length']
+
+
+#Element 5 strain
+e_el5 = (local_disp_el5[3, 0] - local_disp_el5[0, 0])/nae.Elem_5[
+    'length']
+
+
+#Element 6 strain
+e_el6 = (local_disp_el6[3, 0] - local_disp_el6[0, 0])/nae.Elem_6[
+    'length']
+
+
+#Element 7 strain
+e_el7 = (local_disp_el7[3, 0] - local_disp_el7[0, 0])/nae.Elem_7[
+    'length']
+
+
+#Element 8 strain
+e_el8 = (local_disp_el8[3, 0] - local_disp_el8[0, 0])/nae.Elem_8[
+    'length']
+
+
+#Element 9 strain
+e_el9 = (local_disp_el9[3, 0] - local_disp_el9[0, 0])/nae.Elem_9[
+    'length']
+
+
+#Element 10 strain
+e_el10 = (local_disp_el10[3, 0] - local_disp_el10[0, 0])/nae.Elem_10[
+    'length']
+
+
+#Element 11 strain
+e_el11 = (local_disp_el11[3, 0] - local_disp_el11[0, 0])/nae.Elem_11[
+    'length']
+
+
+#Element 12 strain
+e_el12 = (local_disp_el12[3, 0] - local_disp_el12[0, 0])/nae.Elem_12[
+    'length']
+
+
+#Element 13 strain
+e_el13 = (local_disp_el13[3, 0] - local_disp_el13[0, 0])/nae.Elem_13[
+    'length']
+
 a = 1
---
+++ stress_and_forces_frames.py
@@ -0,0 +1,17 @@
+import displacement_calcs_frames as dcf
+E = 73.1*(10**9)
+
+stress_el2 = E*dcf.e_el2
+stress_el3 = E*dcf.e_el3
+stress_el4 = E*dcf.e_el4
+stress_el5 = E*dcf.e_el5
+stress_el6 = E*dcf.e_el6
+stress_el7 = E*dcf.e_el7
+stress_el8 = E*dcf.e_el8
+stress_el9 = E*dcf.e_el9
+stress_el10 = E*dcf.e_el10
+stress_el11 = E*dcf.e_el11
+stress_el12 = E*dcf.e_el12
+stress_el13 = E*dcf.e_el13
+
+a = 1
